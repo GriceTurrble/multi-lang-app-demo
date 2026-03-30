7b84a940ec5377379f552cbc1a9dbe5efe8913d9
Galen Rice <griceturrble@protonmail.com>
2026-03-30 12:21:29 -0400

    ref: simplify test
    We don't have more setup needed,
    so we just run this one with each expected part.

--- integration/tests/home.spec.ts
+++ integration/tests/home.spec.ts
@@ -1,14 +1,8 @@
 import { test, expect } from "fixtures/HomePage";
 
-test("home page is shown", async ({ homePage }) => {
+test("home page controls are shown", async ({ homePage }) => {
   await expect(homePage.heading()).toBeVisible();
-});
-
-test("login and register controls are present", async ({ homePage }) => {
   await expect(homePage.loginLink()).toBeVisible();
   await expect(homePage.registerLink()).toBeVisible();
-});
-
-test("browse posts button shown", async ({ homePage }) => {
   await expect(homePage.browsePostsLink()).toBeVisible();
 });
