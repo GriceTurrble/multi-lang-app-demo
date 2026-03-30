57398ed90292a8387a3ac1a61f37810825376541
Galen Rice <griceturrble@protonmail.com>
2026-03-30 10:21:17 -0400

    tests: expand home page tests
    Written by hand so I can get a feel for things. :)

--- integration/tests/home.spec.ts
+++ integration/tests/home.spec.ts
@@ -3,6 +3,17 @@ import { test, expect } from "@playwright/test";
 test("home page is shown", async ({ page }) => {
   await page.goto("/");
   await expect(
-    page.getByRole("heading", { name: "Welcome to MLAD Forum" }),
+    page.getByRole("heading", { name: "Welcome to MLAD Forum" })
   ).toBeVisible();
+});
+
+test("login and register controls are present", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.getByRole("link", { name: "Login" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Register" })).toBeVisible();
+});
+
+test("browse posts button shown", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.getByRole("link", { name: "Browse Posts" })).toBeVisible();
 });
