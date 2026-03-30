156926f883223dee0d2c460a6872b46a16bcee17
Galen Rice <griceturrble@protonmail.com>
2026-03-30 12:54:04 -0400

    chore: comment for maintenance

--- frontends/nextjs/components/ui/Username.tsx
+++ frontends/nextjs/components/ui/Username.tsx
@@ -8,6 +8,8 @@ type Props = {
 };
 
 export function Username({ username, isCurrentUser }: Props) {
+  // NOTE your editor will probably not highlight if you have conflicts in Tailwind styles listed here.
+  // Use caution, please!
   const sameUserStyle  = "text-blue-600 ring-blue-200 dark:text-blue-400 dark:ring-blue-800";
   const otherUserStyle = "text-gray-700 ring-gray-200 dark:text-gray-300 dark:ring-gray-700";
   return (
